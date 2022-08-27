e1d437336dfa3407f96e15010cacb275de8af175
NikhilPatil9435 <np9435@gmail.com>
2022-08-27 15:27:19 +0530

    code added for multiplication

--- calculator.py
+++ calculator.py
@@ -10,3 +10,7 @@ def substraction(num1,num2):
 ans=substraction(num1,num2)
 print(f"The substraction of {num1} and {num2} is :{ans}")
 
+def multiplication(num1,num2):
+    return num1*num2
+ans=multiplication(num1,num2)
+print(f"The multiplication of {num1} and {num2} is :{ans}")
