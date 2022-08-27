fede3ae493e6f0c550bf94732631071cb104e48c
NikhilPatil9435 <np9435@gmail.com>
2022-08-27 15:58:46 +0530

    asch

--- calculator.py
+++ calculator.py
@@ -1,19 +1,21 @@
 num1=10
 num2=30
+# code for addtion
 def addtion(num1,num2):
     return num1+num2
 ans=addtion(num1,num2)
 print(f"The addition of {num1} and {num2} is :{ans}")
-
+# code for substraction
 def substraction(num1,num2):
     return num1-num2
 ans=substraction(num1,num2)
 print(f"The substraction of {num1} and {num2} is :{ans}")
-
+#code for multiplication
 def multiplication(num1,num2):
     return num1*num2
 ans=multiplication(num1,num2)
 print(f"The multiplication of {num1} and {num2} is :{ans}")
+#code for division
 def division(num1,num2):
     return num1/num2
 ans=division(num1,num2)
