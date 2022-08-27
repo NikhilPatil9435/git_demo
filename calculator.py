9d3023e576fd1e2d4fe3f21e92ff2dad37c8ae41
NikhilPatil9435 <np9435@gmail.com>
2022-08-27 15:35:45 +0530

    code added for division

--- calculator.py
+++ calculator.py
@@ -10,3 +10,7 @@ def substraction(num1,num2):
 ans=substraction(num1,num2)
 print(f"The substraction of {num1} and {num2} is :{ans}")
 
+def division(num1,num2):
+    return num1/num2
+ans=division(num1,num2)
+print(f"The division of {num1} and {num2} is :{ans}")
